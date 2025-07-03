c510fa0fd6051512b7c6b39f4f47262aac81a103
Stefan <58789967+stefanleoussis@users.noreply.github.com>
2025-07-03 16:08:02 -0700

    Initial Commit

---
+++ ScreenTimeWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenTimeWidget
+//
+//  Created by Stefan Leoussis on 2025-07-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenTimeWidget/ScreenTimeWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeWidgetApp.swift
+//  ScreenTimeWidget
+//
+//  Created by Stefan Leoussis on 2025-07-03.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
